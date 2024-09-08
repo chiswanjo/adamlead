439802ea8e32e936a33a33f7cd094f9b44376bb2
chiswanjo <153459473+chiswanjo@users.noreply.github.com>
2024-09-08 19:39:58 +0200

    Create Leads.js

---
+++ src/pages/Leads.js
@@ -0,0 +1,23 @@
+// src/pages/Leads.js
+
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const Leads = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Leads | Pay-Per-Lead Platform</title>
+        <meta name="description" content="Browse and purchase high-quality leads to grow your business." />
+      </Helmet>
+      
+      <div className="leads-container">
+        <h1>Available Leads</h1>
+        {/* Lead listing will go here */}
+        <p>Here you can browse the available leads.</p>
+      </div>
+    </>
+  );
+};
+
+export default Leads;
