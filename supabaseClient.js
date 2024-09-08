d661a0e1f8780ec8efbd2972b41f345e520b5101
chiswanjo <153459473+chiswanjo@users.noreply.github.com>
2024-09-08 19:36:48 +0200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// src/supabaseClient.js
-
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://lrhkmcfqblgrqruhnwmx.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
